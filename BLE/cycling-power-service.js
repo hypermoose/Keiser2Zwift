0e72c3d73ab4658d00657006d7676ec0921628de
Gary Caldwell <gacald@live.com>
2021-01-23 09:20:49 -0800

    Update cycling-power-service.js
    Switch back to the standard UUID for cycling power as discovered by @djwasser

--- BLE/cycling-power-service.js
+++ BLE/cycling-power-service.js
@@ -9,8 +9,8 @@ class CyclingPowerService extends Bleno.PrimaryService {
   constructor() {
     let powerMeasurement = new CyclingPowerMeasurementCharacteristic();
     super({
-        // uuid: '1818',
-		uuid: '1515',
+        uuid: '1818',
+	//uuid: '1515',
         characteristics: [
           powerMeasurement,
           new StaticReadCharacteristic('2A65', 'Cycling Power Feature', [0x08, 0, 0, 0]), // 0x08 - crank revolutions
